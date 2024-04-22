42f49b8e61f8549249e8f576cf72b0e01abf4b88
laurentheirendt <laurent.heirendt@nablify.com>
2024-04-22 10:08:06 +0200

    add version 1.0

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+* Start Bootstrap - nablify v1.0.0 (https://nablify.com)
+* Copyright 2023-2024 nablify.com team
+* Licensed under Apache 2.0 (https://github.com/nablify/web/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
